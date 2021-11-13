b80f8730494389e93464449d0782ec5aac539c8c
Kazyg <guilherme_severo@estudante.sc.senai.br>
2021-11-13 17:08:40 -0300

    botao adicionar tarefa
    parte inicial do codigo junto com o botão de adicionar tarefa

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+
+function botaoAdd(){
+
+    document.getElementById("botao").innerHTML =('<button id="salvar" value="Adicionar Tarefa" onclick="salvar()">Adicionar Tarefa</button>')
+    document.getElementById("formularioLista").innerHTML =('<form> Titulo: <input type="text" id="titulo"><br> descrição: <input type="text" id="descricao"><br> Data para finalizar: <input type="date" id="data"></form>')
+    
+}
+
+
+function voltaBotao(){
+    document.getElementById("botao").innerHTML =('<button id="adicionar" value="Adicionar Tarefa" onclick="botaoAdd()">Adicionar Tarefa</button>')
+    document.getElementById("formularioLista").innerHTML =('')
+
+}
---
+++ script/scriptForm.js
@@ -0,0 +1,13 @@
+function salvar(){
+const titulo = document.getElementById('titulo');
+const descricao = document.getElementById('descricao');
+const data = document.getElementById('data');
+
+voltaBotao()
+const tituloV = titulo.value.trim();
+const descricaoV = descricao.value.trim();
+const dataV = data.value.trim();
+
+    let tarefa = {"titulo":tituloV, "descricao":descricaoV, "data":dataV};
+    console.log(tarefa)
+}
